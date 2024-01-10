872bfb0db07faff471b38aede911b9a3f4eaabfb
Banuprakash <banuprakashc@yahoo.co.in>
2024-01-10 10:20:24 +0530

    day 3

--- productapp/src/components/CartList.tsx
+++ productapp/src/components/CartList.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
-
-export default function CartList() {
+import React, { useContext } from 'react'
+import Cart from '../model/Cart'
+import { CartContext } from '../context/CartContext'
+import { Button, Container } from 'react-bootstrap'
+type AppProps = {
+  product: Cart
+}
+export default function CartList({ product }: AppProps) {
+  let { increment } = useContext(CartContext)
   return (
-    <div>CartList</div>
+    <div className='row'>
+      <div className='col-md-2'>
+        <img src={product.image} style={{ "width": "50px" }} />
+      </div>
+      <div className='col-md-2'>
+        {product.title}
+      </div>
+      <div className='col-md-4'>
+        <Button>-</Button>
+          {product.quantity}
+        <Button onClick={() => increment(product.id)}>+</Button>
+      </div>
+      <div className='col-md-2'>
+        {product.price}
+      </div>
+      <div className='col-md-2'>
+        {product.amount}
+      </div>
+    </div>
   )
 }
